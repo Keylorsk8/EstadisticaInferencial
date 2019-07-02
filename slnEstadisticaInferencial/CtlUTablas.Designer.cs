df71ba963c748b6453eea2a966f2e4caf51929d0
KEYLOR ALBERTO MARTINEZ RODRIGUEZ <komartinez@est.utn.ac.cr>
2019-07-02 16:04:12 -0600

    Interfaz Principal creada

---
+++ slnEstadisticaInferencial/CtlUTablas.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace slnEstadisticaInferencial
+{
+    partial class CtlUTablas
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblTitulo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LblTitulo
+            // 
+            this.LblTitulo.AutoSize = true;
+            this.LblTitulo.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblTitulo.Location = new System.Drawing.Point(3, 0);
+            this.LblTitulo.Name = "LblTitulo";
+            this.LblTitulo.Size = new System.Drawing.Size(164, 24);
+            this.LblTitulo.TabIndex = 1;
+            this.LblTitulo.Text = "Listas de Carne";
+            // 
+            // CtlUTablas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.LblTitulo);
+            this.Name = "CtlUTablas";
+            this.Size = new System.Drawing.Size(906, 412);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblTitulo;
+    }
+}
--- slnEstadisticaInferencial/Menu Principal.Designer.cs
+++ slnEstadisticaInferencial/Menu Principal.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace slnEstadisticaInferencial
 {
-    partial class Form1
+    partial class FrmMenuPrincipal
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,50 +28,219 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMenuPrincipal));
+            this.PnlSideBar = new System.Windows.Forms.Panel();
+            this.PnlSidePanel = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.BtnTablas = new System.Windows.Forms.Button();
+            this.BtnInicio = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.PnlNavBar = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.BtnSalir = new System.Windows.Forms.Button();
+            this.LblProyecto = new System.Windows.Forms.Label();
+            this.LblTitulo = new System.Windows.Forms.Label();
+            this.ctlUTablas1 = new slnEstadisticaInferencial.CtlUTablas();
+            this.ctluInicio1 = new slnEstadisticaInferencial.CtluInicio();
+            this.PnlSideBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // PnlSideBar
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(230, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Proyecto Programado de Estadistica Inferencial";
+            this.PnlSideBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.PnlSideBar.Controls.Add(this.PnlSidePanel);
+            this.PnlSideBar.Controls.Add(this.button4);
+            this.PnlSideBar.Controls.Add(this.button3);
+            this.PnlSideBar.Controls.Add(this.button2);
+            this.PnlSideBar.Controls.Add(this.BtnSalir);
+            this.PnlSideBar.Controls.Add(this.BtnTablas);
+            this.PnlSideBar.Controls.Add(this.BtnInicio);
+            this.PnlSideBar.Controls.Add(this.panel1);
+            this.PnlSideBar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.PnlSideBar.Location = new System.Drawing.Point(0, 0);
+            this.PnlSideBar.Name = "PnlSideBar";
+            this.PnlSideBar.Size = new System.Drawing.Size(169, 554);
+            this.PnlSideBar.TabIndex = 3;
             // 
-            // label2
+            // PnlSidePanel
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(16, 43);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(60, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Integrantes";
+            this.PnlSidePanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.PnlSidePanel.Location = new System.Drawing.Point(3, 75);
+            this.PnlSidePanel.Name = "PnlSidePanel";
+            this.PnlSidePanel.Size = new System.Drawing.Size(10, 59);
+            this.PnlSidePanel.TabIndex = 0;
             // 
-            // label3
+            // button4
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(16, 69);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(74, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Gloriana Vega";
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(14, 512);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(29, 30);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "?";
+            this.button4.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // button3
+            // 
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.Location = new System.Drawing.Point(14, 336);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(157, 59);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "xxxxxx";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(14, 249);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(157, 59);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "xxxxxx";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // BtnTablas
+            // 
+            this.BtnTablas.FlatAppearance.BorderSize = 0;
+            this.BtnTablas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnTablas.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnTablas.ForeColor = System.Drawing.Color.White;
+            this.BtnTablas.Image = ((System.Drawing.Image)(resources.GetObject("BtnTablas.Image")));
+            this.BtnTablas.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnTablas.Location = new System.Drawing.Point(14, 162);
+            this.BtnTablas.Name = "BtnTablas";
+            this.BtnTablas.Size = new System.Drawing.Size(157, 59);
+            this.BtnTablas.TabIndex = 7;
+            this.BtnTablas.Text = "Tablas";
+            this.BtnTablas.UseVisualStyleBackColor = true;
+            this.BtnTablas.Click += new System.EventHandler(this.BtnTablas_Click);
+            // 
+            // BtnInicio
+            // 
+            this.BtnInicio.FlatAppearance.BorderSize = 0;
+            this.BtnInicio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnInicio.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnInicio.ForeColor = System.Drawing.Color.White;
+            this.BtnInicio.Image = ((System.Drawing.Image)(resources.GetObject("BtnInicio.Image")));
+            this.BtnInicio.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnInicio.Location = new System.Drawing.Point(14, 75);
+            this.BtnInicio.Name = "BtnInicio";
+            this.BtnInicio.Size = new System.Drawing.Size(157, 59);
+            this.BtnInicio.TabIndex = 6;
+            this.BtnInicio.Text = "Inicio";
+            this.BtnInicio.UseVisualStyleBackColor = true;
+            this.BtnInicio.Click += new System.EventHandler(this.BtnInicio_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panel1.Location = new System.Drawing.Point(183, 111);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 4;
+            // 
+            // PnlNavBar
+            // 
+            this.PnlNavBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.PnlNavBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PnlNavBar.Location = new System.Drawing.Point(169, 0);
+            this.PnlNavBar.Name = "PnlNavBar";
+            this.PnlNavBar.Size = new System.Drawing.Size(906, 21);
+            this.PnlNavBar.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(170, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(148, 115);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // BtnSalir
+            // 
+            this.BtnSalir.FlatAppearance.BorderSize = 0;
+            this.BtnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnSalir.Image = ((System.Drawing.Image)(resources.GetObject("BtnSalir.Image")));
+            this.BtnSalir.Location = new System.Drawing.Point(4, 3);
+            this.BtnSalir.Name = "BtnSalir";
+            this.BtnSalir.Size = new System.Drawing.Size(39, 38);
+            this.BtnSalir.TabIndex = 6;
+            this.BtnSalir.UseVisualStyleBackColor = true;
+            this.BtnSalir.Click += new System.EventHandler(this.BtnSalir_Click);
+            // 
+            // LblProyecto
+            // 
+            this.LblProyecto.AutoSize = true;
+            this.LblProyecto.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblProyecto.Location = new System.Drawing.Point(324, 41);
+            this.LblProyecto.Name = "LblProyecto";
+            this.LblProyecto.Size = new System.Drawing.Size(327, 24);
+            this.LblProyecto.TabIndex = 7;
+            this.LblProyecto.Text = "Proyecto Estadística Inferencial";
+            // 
+            // LblTitulo
+            // 
+            this.LblTitulo.AutoSize = true;
+            this.LblTitulo.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblTitulo.Location = new System.Drawing.Point(325, 75);
+            this.LblTitulo.Name = "LblTitulo";
+            this.LblTitulo.Size = new System.Drawing.Size(297, 21);
+            this.LblTitulo.TabIndex = 8;
+            this.LblTitulo.Text = "Estadísticas mundiales sobre la carne";
+            // 
+            // ctlUTablas1
+            // 
+            this.ctlUTablas1.Location = new System.Drawing.Point(169, 142);
+            this.ctlUTablas1.Name = "ctlUTablas1";
+            this.ctlUTablas1.Size = new System.Drawing.Size(906, 412);
+            this.ctlUTablas1.TabIndex = 10;
+            // 
+            // ctluInicio1
+            // 
+            this.ctluInicio1.Location = new System.Drawing.Point(169, 142);
+            this.ctluInicio1.Name = "ctluInicio1";
+            this.ctluInicio1.Size = new System.Drawing.Size(906, 412);
+            this.ctluInicio1.TabIndex = 9;
+            // 
+            // FrmMenuPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(398, 342);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Name = "Form1";
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.ClientSize = new System.Drawing.Size(1075, 554);
+            this.Controls.Add(this.ctlUTablas1);
+            this.Controls.Add(this.ctluInicio1);
+            this.Controls.Add(this.LblTitulo);
+            this.Controls.Add(this.LblProyecto);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.PnlNavBar);
+            this.Controls.Add(this.PnlSideBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmMenuPrincipal";
+            this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Menu Principal";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.PnlSideBar.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -79,9 +248,21 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel PnlSideBar;
+        private System.Windows.Forms.Panel PnlNavBar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button BtnTablas;
+        private System.Windows.Forms.Button BtnInicio;
+        private System.Windows.Forms.Button BtnSalir;
+        private System.Windows.Forms.Label LblProyecto;
+        private System.Windows.Forms.Label LblTitulo;
+        private System.Windows.Forms.Button button4;
+        private CtluInicio ctluInicio1;
+        private System.Windows.Forms.Panel PnlSidePanel;
+        private CtlUTablas ctlUTablas1;
     }
 }
 
